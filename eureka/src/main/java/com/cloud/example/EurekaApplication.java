93936ee6bc3f8728b7c7e935b2930193af3682de
李东东 <lidongdong@gxcards.com>
2019-04-02 15:46:46 +0800

    补充

--- eureka/src/main/java/com/cloud/example/EurekaApplication.java
+++ eureka/src/main/java/com/cloud/example/EurekaApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 注册中心
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 
 }
