a7ddbf19fa0bfe6a19373c3d4a19cceec1cbe815
李东东 <lidongdong@gxcards.com>
2019-05-24 10:05:36 +0800

    锁互斥后报错

--- dao/src/main/java/com/cloud/example/domain/UserDAO.java
+++ dao/src/main/java/com/cloud/example/domain/UserDAO.java
@@ -1,8 +1,12 @@
 package com.cloud.example.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.annotation.Version;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -33,6 +37,8 @@ public class UserDAO {
     @Column(name = "phone_no", columnDefinition = "varchar(32) COMMENT '手机号'")
     private String phoneNo;
 
+    @Transient //此字段不与数据库关联
+    @Version//此字段加上乐观锁
     @Column(name = "password", columnDefinition = "varchar(32) COMMENT '登陆密码'")
     private String password;
 
@@ -41,6 +47,10 @@ public class UserDAO {
     public Date createDate;
 
     @UpdateTimestamp
+    //出参时间格式化
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    //入参时，请求报文只需要传入yyyymmddhhmmss字符串进来，则自动转换为Date类型数据
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @Column(name = "update_date")
     public Date updateDate;
 }
