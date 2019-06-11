13a12445d01696ab11f93622475f195abb70462c
李东东 <lidongdong@gxcards.com>
2019-06-11 13:56:10 +0800

    更新seata配置

---
+++ example-seata/example-account/src/main/java/com/seata/example/controller/AccountController.java
@@ -0,0 +1,28 @@
+package com.seata.example.controller;
+
+import com.seata.example.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+/**
+ * Description：
+ *
+ * @author fangliangsheng
+ * @date 2017/12/25
+ */
+@RestController
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @RequestMapping("/debit")
+    public Boolean debit(String userId, BigDecimal money) {
+        accountService.debit(userId, money);
+
+        return true;
+    }
+}
