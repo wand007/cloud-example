d0affebd700fdc1d3a8dcffcc672a64b0534772b
李东东 <lidongdong@gxcards.com>
2019-06-12 11:59:36 +0800

    Nacos

---
+++ example-nacos/example-consumer/src/main/java/com/nacos/example/ExampleConsumerApplication.java
@@ -0,0 +1,27 @@
+package com.nacos.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient // 启用服务发现
+@EnableFeignClients // 启用 Feign
+public class ExampleConsumerApplication {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleConsumerApplication.class, args);
+
+    }
+
+}
